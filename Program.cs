7b5ebb173f0f02306f2bd669b56657dcd6afc3c2
oliviaposadas-collab <olivia.posadas@atlaschoo.com>
2026-02-15 15:32:29 -0600

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter an integer: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a decimal: ");
+            decimal num2 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Enter a boolean (true/false): ");
+            bool val = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine($"You entered {num1}, {num2}, and {val}");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("FormatException: input was not in a correct format.");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unhandled exception: {ex.GetType().Name} - {ex.Message}");
+        }
+    }
+}
