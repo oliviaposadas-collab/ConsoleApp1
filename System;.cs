2dadd70342aa14a77d90a2f5adb1c33e3ed592b7
oliviaposadas-collab <olivia.posadas@atlaschoo.com>
2026-02-16 16:47:56 -0600

    first commit

---
+++ System;.cs
@@ -0,0 +1,16 @@
+System;
+
+Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}else
+{
+    Console.WriteLine("This code is not running in a C# environment.");
+}      
+
